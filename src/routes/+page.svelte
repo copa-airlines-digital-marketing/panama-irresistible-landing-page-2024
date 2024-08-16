45770c5492a4eb3e9835bf9e1faab8a0aa2a2d74
Jose Von Chong <javch1995@gmail.com>
2024-08-16 16:49:29 -0500

    ready

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
 
 <div class=" grid grid-cols-[16px_1fr_16px] grid-rows-[16px_2fr_1fr_1.5fr_16px] sm:grid-cols-[40%_20%_40%] sm:grid-rows-[1fr_1fr] overflow-hidden h-[140vw] sm:h-[32vw] ">
     <div class="relative col-start-1 col-end-4 row-start-1 row-end-4  sm:col-start-1 sm:col-end-3 sm:row-start-1 sm:row-end-3 overflow-hidden">
-        <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresistible-hero-tucan.jpg" alt="tucan en un arbol" class="block h-full sm:absolute w-full md:h-auto md:top-[50%] md:translate-y-[-50%] overflow-hidden" >
+        <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresistible-hero-tucan.jpg" alt="Toucan in a tree" class="block h-full sm:absolute w-full md:h-auto md:top-[50%] md:translate-y-[-50%] overflow-hidden object-cover object-left" >
     </div>
   
     <div class="relative xs:col-start-1 xs:col-end-5 xs:row-start-4 xs:row-end-6  sm:col-start-3 sm:col-end-4 sm:row-start-1 sm:row-end-3 overflow-hidden ">
         <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresitible-hero-city.jpg" alt="Vista al skyline de la ciudad de Panamá" class="z-[-0] overflow-hidden sm:h-[158%] sm:max-w-none sm:right-0 xs:translate-y-[-19%] sm:translate-x-[-36%] lg:translate-y-[-24%] w-auto ">
     </div>
-    <div class="mix-blend-multiply opacity-[0.64] bg-primary-dark  xs:col-start-1 xs:col-end-4 xs:row-start-3 xs:row-end-4  sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-3 relative">sdasd</div>
-    <div class="mix-blend-color bg-primary-dark xs:col-start-1 xs:col-end-4 xs:row-start-3 xs:row-end-4  sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-3 relative">sdasds</div>
+    <div class="mix-blend-multiply opacity-[0.64] bg-primary-dark  xs:col-start-1 xs:col-end-4 xs:row-start-3 xs:row-end-4  sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-3 relative">&nbsp;</div>
+    <div class="mix-blend-color bg-primary-dark xs:col-start-1 xs:col-end-4 xs:row-start-3 xs:row-end-4  sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-3 relative">&nbsp;</div>
   
   
     <h1 class="col-start-1 col-end-4 row-start-3 row-end-4 sm:row-start-1 sm:row-end-3 relative text-center self-center font-gilroy text-u7 uppercase text-green-50 font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-7xl">
@@ -18,7 +18,7 @@
         <span class="spacingTitle">Irresistible</span>
     </h1>
   
-    <div class="xs:row-start-4 xs:row-end-5 xs:col-start-1 xs:col-end-4 sm:col-start-3 sm:col-end-3 sm:row-start-1 sm:row-end-2 z-0 self-center sm:self-end h-[130px] w-[130px] l justify-center sm:m-20 md:m-12 lg:ml-20 g:h-[200px] lg:w-[200px]">
+    <div class="xs:row-start-4 xs:row-end-5 xs:col-start-1 xs:col-end-4 sm:col-start-3 sm:col-end-3 sm:row-start-1 sm:row-end-2 z-0 self-center sm:self-end h-[130px] w-[130px] justify-center sm:m-20 md:m-12 lg:ml-20 g:h-[200px] lg:w-[200px]">
         <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresistible-stamp.svg" alt="A una tarifa imperdible" class="h-auto">
     </div>
   </div>
@@ -36,3 +36,90 @@
         desde el<br><span class="text-primary ">30 de noviembre del 2024</span>
     </h3>
   </div>
+
+
+<!--ENDS ES-->
+  
+
+<div class=" grid grid-cols-[16px_1fr_16px] grid-rows-[16px_2fr_1fr_1.5fr_16px] sm:grid-cols-[40%_20%_40%] sm:grid-rows-[1fr_1fr] overflow-hidden h-[140vw] sm:h-[32vw] ">
+    <div class="relative col-start-1 col-end-4 row-start-1 row-end-4  sm:col-start-1 sm:col-end-3 sm:row-start-1 sm:row-end-3 overflow-hidden">
+        <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresistible-hero-tucan.jpg" alt="Toucan in a tree" class="block h-full sm:absolute w-full md:h-auto md:top-[50%] md:translate-y-[-50%] overflow-hidden" >
+    </div>
+  
+    <div class="relative xs:col-start-1 xs:col-end-5 xs:row-start-4 xs:row-end-6  sm:col-start-3 sm:col-end-4 sm:row-start-1 sm:row-end-3 overflow-hidden ">
+        <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresitible-hero-city.jpg" alt="Vista al skyline de la ciudad de Panamá" class="z-[-0] overflow-hidden sm:h-[158%] sm:max-w-none sm:right-0 xs:translate-y-[-19%] sm:translate-x-[-36%] lg:translate-y-[-24%] w-auto ">
+    </div>
+    <div class="mix-blend-multiply opacity-[0.64] bg-primary-dark  xs:col-start-1 xs:col-end-4 xs:row-start-3 xs:row-end-4  sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-3 relative">&nbsp;</div>
+    <div class="mix-blend-color bg-primary-dark xs:col-start-1 xs:col-end-4 xs:row-start-3 xs:row-end-4  sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-3 relative">&nbsp;</div>
+  
+  
+    <h1 class="col-start-1 col-end-4 row-start-3 row-end-4 sm:row-start-1 sm:row-end-3 relative text-center self-center font-gilroy text-u7 uppercase text-green-50 font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-7xl">
+        <span class="spacingTitle">Irresistible</span>
+        <br />
+        <span class="spacingTitle">Panama</span>
+    </h1>
+  
+    <div class="xs:row-start-4 xs:row-end-5 xs:col-start-1 xs:col-end-4 sm:col-start-3 sm:col-end-3 sm:row-start-2 sm:row-end-2 z-0 self-center sm:self-end h-[130px] w-[130px] justify-center sm:m-20 md:m-12 lg:ml-20 g:h-[200px] lg:w-[200px]">
+        <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresistible-stamp-en.svg" alt="A una tarifa imperdible" class="h-auto">
+    </div>
+  </div>
+
+
+  <div id="panamaIrresistible" class="contaienr mx-auto py-5">
+    <h2 class="text-center text-primary font-gilroy text-md text-u2">
+        <strong>Find these unmissable fares to <span class="text-secondary">Panama</span></strong>
+    </h2>
+    <h3 class="text-center text-grey-600 font-gilroy text-u1 text-md">Take advantage and book from today until
+        <br><span class="text-primary">September 15th, 2024</span>
+    </h3>
+    
+    <h3 class="text-center text-grey-600 font-gilroy text-u1 text-md">to travel from today until
+        <br><span class="text-primary">November 30th, 2024</span>
+    </h3>
+    
+  </div>
+
+<!--ENDS EN-->
+
+  <div class=" grid grid-cols-[16px_1fr_16px] grid-rows-[16px_2fr_1fr_1.5fr_16px] sm:grid-cols-[40%_20%_40%] sm:grid-rows-[1fr_1fr] overflow-hidden h-[140vw] sm:h-[32vw] ">
+    <div class="relative col-start-1 col-end-4 row-start-1 row-end-4  sm:col-start-1 sm:col-end-3 sm:row-start-1 sm:row-end-3 overflow-hidden">
+        <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresistible-hero-tucan.jpg" alt="Toucan in a tree" class="block h-full sm:absolute w-full md:h-auto md:top-[50%] md:translate-y-[-50%] overflow-hidden" >
+    </div>
+  
+    <div class="relative xs:col-start-1 xs:col-end-5 xs:row-start-4 xs:row-end-6  sm:col-start-3 sm:col-end-4 sm:row-start-1 sm:row-end-3 overflow-hidden ">
+        <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresitible-hero-city.jpg" alt="Vista al skyline de la ciudad de Panamá" class="z-[-0] overflow-hidden sm:h-[158%] sm:max-w-none sm:right-0 xs:translate-y-[-19%] sm:translate-x-[-36%] lg:translate-y-[-24%] w-auto ">
+    </div>
+    <div class="mix-blend-multiply opacity-[0.64] bg-primary-dark  xs:col-start-1 xs:col-end-4 xs:row-start-3 xs:row-end-4  sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-3 relative">&nbsp;</div>
+    <div class="mix-blend-color bg-primary-dark xs:col-start-1 xs:col-end-4 xs:row-start-3 xs:row-end-4  sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-3 relative">&nbsp;</div>
+  
+  
+    <h1 class="col-start-1 col-end-4 row-start-3 row-end-4 sm:row-start-1 sm:row-end-3 relative text-center self-center font-gilroy text-u7 uppercase text-green-50 font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-7xl">
+        <span class="spacingTitle">Irresistivel</span>
+        <br />
+        <span class="spacingTitle">Panama</span>
+    </h1>
+  
+    <div class="xs:row-start-4 xs:row-end-5 xs:col-start-1 xs:col-end-4 sm:col-start-3 sm:col-end-3 sm:row-start-2 sm:row-end-2 z-0 self-center sm:self-end h-[130px] w-[130px] justify-center sm:m-20 md:m-12 lg:ml-20 g:h-[200px] lg:w-[200px]">
+        <img src="https://www.copaair.com/promotions/airtrfx-lp/panama-increible/panama-irresistible-stamp-pt.svg" alt="A una tarifa imperdible" class="h-auto">
+    </div>
+  </div>
+
+
+  <div id="panamaIrresistible" class="contaienr mx-auto py-5">
+    <h2 class="text-center text-primary font-gilroy text-md text-u2">
+        <strong>Encontre essas tarifas imperdíveis para o <span class="text-secondary">Panamá</span></strong>
+    </h2>
+    <h3 class="text-center text-grey-600 font-gilroy text-u1 text-md">Aproveite e compre de hoje até
+        <br><span class="text-primary">15 de setembro de 2024</span>
+    </h3>
+    
+    <h3 class="text-center text-grey-600 font-gilroy text-u1 text-md">para viajar de hoje até
+        <br><span class="text-primary">30 de novembro de 2024</span>
+    </h3>
+    
+  </div>
+
+
+
+
+  
